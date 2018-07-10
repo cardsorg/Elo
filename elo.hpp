b2eb4f157913d4b6a64939442c0e47fced44573d
atlanticoysters <37308183+atlanticoysters@users.noreply.github.com>
2018-07-09 21:52:01 -0400

    Add rating difference estimator from results against one opponent.

--- elo.hpp
+++ elo.hpp
@@ -21,14 +21,16 @@ THE SOFTWARE.
 
 #include <cmath>
 #include <functional>
+#include <stdexcept>
 #include <utility>
 #include <vector>
 
 namespace Elo {
 
-double WIN = 1;
-double DRAW = 0.5;
-double LOSS = 0;
+const double WIN = 1;
+const double DRAW = 0.5;
+const double LOSS = 0;
+const double LOGISTIC_DIVISOR = 400 / std::log(10);
 
 double round_places(double x, double places) {
 	if (x == 0) {
@@ -76,17 +78,17 @@ class Player;
 class Match;
 
 struct Configuration {
-	Distribution& dist;
 	std::function<double(Player&)> calculate_k;
+	Distribution& dist;
 
-	Configuration(Distribution& initial_distribution, double initial_k):
-		dist(initial_distribution), calculate_k([initial_k](Player& p) {return initial_k;}) {};
+	Configuration(double initial_k, Distribution& initial_distribution):
+		calculate_k([initial_k](Player& p) {return initial_k;}), dist(initial_distribution) {};
 
-	Configuration(Distribution& initial_distribution, std::function<double(Player&)> initial_calculate_k):
-		dist(initial_distribution), calculate_k(initial_calculate_k) {};
+	Configuration(std::function<double(Player&)> initial_calculate_k, Distribution& initial_distribution):
+		calculate_k(initial_calculate_k), dist(initial_distribution) {};
 };
 
-Configuration default_configuration(default_distribution, 32);
+Configuration default_configuration(32, default_distribution);
 
 class Player {
 	std::vector<Match> matches;
@@ -130,4 +132,127 @@ struct Match {
 		player_b.add_match(*this);
 	}
 };
+
+struct IntervalEstimate {
+	double lower;
+	double estimate;
+	double upper;
+	bool lower_infinity = false;
+	bool estimate_infinity = false;
+	bool upper_infinity = false;
+
+};
+
+/* Source: Abramowitz, M. & Stegun, I. (1964).
+Handbook of Mathematical Functions with Formulas, Graphs, and Mathematical Tables.
+Section 26.2.23. */
+double homf_tail(double p) {
+	if (p <= 0 || p >= 1) {
+		throw std::invalid_argument("p must be in [0,1].");
+	}
+
+	double c0 = 2.515517;
+	double c1 = 0.802853;
+	double c2 = 0.010328;
+	double d1 = 1.432788;
+	double d2 = 0.189269;
+	double d3 = 0.001308;
+
+	double t = std::sqrt(std::log(1 / (p * p)));
+	return t - (c0 + c1 * t + c2 * t * t) / (1 + d1 * t + d2 * t * t + d3 * t * t * t);
+}
+
+double quantile(double p) {
+	if (p < 0 || p > 1) {
+		throw std::invalid_argument("p must be in [0,1].");
+	}
+
+	if (p < 0.5) {
+		return -homf_tail(p);
+	} else {
+		return homf_tail(1 - p);
+	}
+}
+
+// x successes in n trials using the Wilson score interval (see also https://arxiv.org/pdf/1303.1288.pdf).
+IntervalEstimate binomial_estimate(double x, double n, double p = 0.95) {
+	if (x < 0) {
+		throw std::invalid_argument("x must be nonnegative.");
+	}
+
+	if (n <= 0) {
+		throw std::invalid_argument("n must be positive.");
+	}
+
+	if (x > n) {
+		throw std::invalid_argument("x must not be greater than n.");
+	}
+
+	IntervalEstimate est;
+	double z = quantile(1 - (1 - p) / 2);
+	double mlp = x / n;
+	double mlq = 1 - mlp;
+	double mid = (x + (z * z) / 2) / (n + z * z);
+	double delta = (z / (n + z * z)) * std::sqrt(mlp * mlq * n + (z * z) / 4);
+
+	est.estimate = mid;
+	est.lower = mid - delta;
+	est.upper = mid + delta;
+	return est;
+}
+
+// Default parameters with the default distribution.
+double logistic_inverse_cdf(double x, double mean = 0, double scale = LOGISTIC_DIVISOR) {
+	if (x <= 0 || x >= 1) {
+		throw std::invalid_argument("x must be in (0,1).");
+	}
+
+	return (mean + scale * std::log(x / (1 - x)));
+}
+
+
+// Rating difference assuming the default Logistic distribution.
+IntervalEstimate estimate_rating_difference(double wins, double draws, double losses, double p = 0.95) {
+	if (wins < 0 || draws < 0 || losses < 0) {
+		throw std::invalid_argument("wins, draws, and losses must be nonnegative.");
+	}
+
+	// Naive solution: 1 draw = 1 win and 1 loss. This is so we can calculate based on a binomial rather than trinomial distribution.
+	wins += draws;
+	losses += draws;
+	double n = wins + losses;
+
+	if (n <= 0) {
+		throw std::invalid_argument("The total number of games must be positive.");
+	}
+
+	IntervalEstimate est;
+
+	if (wins / n == 0 || wins / n == 1) {
+		est.estimate_infinity = true;
+		return est;
+	}
+
+	est = binomial_estimate(wins, n, p);
+
+	if (est.estimate <= 0 || est.estimate >= 1) {
+		est.estimate_infinity = true;
+	} else {
+		est.estimate = logistic_inverse_cdf(est.estimate);
+	}
+
+	if (est.lower <= 0) {
+		est.lower_infinity = true;
+	} else {
+		est.lower = logistic_inverse_cdf(est.lower);
+	}
+
+	if (est.upper >= 1) {
+		est.upper_infinity = true;
+	} else {
+		est.upper = logistic_inverse_cdf(est.upper);
+	}
+
+	return est;
+}
 }
